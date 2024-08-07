149ab2b6a366bc71f87ab3852874a3f0da8890ec
Jeb50 <119408915+Pranavi-Kayapati@users.noreply.github.com>
2024-08-07 16:03:02 +0530

    add

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const cartRoutes = require("./routes/cartRouter");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/menu", menuRoutes);
